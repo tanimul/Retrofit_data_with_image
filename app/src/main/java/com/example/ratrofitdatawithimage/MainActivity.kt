b59fb6b930888b329fcf9f3c78551dde3389facd
tanimul <tanimulislam.fuad@gmail.com>
2022-04-02 17:52:56 +0600

    image get

--- app/src/main/java/com/example/ratrofitdatawithimage/MainActivity.kt
+++ app/src/main/java/com/example/ratrofitdatawithimage/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.ratrofitdatawithimage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.ratrofitdatawithimage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    private val image_rec_code = 1
+    private val filepath_uri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSelectImage.setOnClickListener {
+            mGetContent.launch("image/*")
+        }
     }
+
+    private val mGetContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            Glide
+                .with(this)
+                .load(uri)
+                .centerCrop()
+                .into(binding.ivImage);
+
+        }
 }
