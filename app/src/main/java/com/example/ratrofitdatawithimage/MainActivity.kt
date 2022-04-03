99379ef7fb854d73eeed76be11196f5dedb3253a
tanimul <tanimulislam.fuad@gmail.com>
2022-04-03 22:47:28 +0600

    -

--- app/src/main/java/com/example/ratrofitdatawithimage/MainActivity.kt
+++ app/src/main/java/com/example/ratrofitdatawithimage/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.ratrofitdatawithimage
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.ratrofitdatawithimage.databinding.ActivityMainBinding
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
-    private val image_rec_code = 1
-    private val filepath_uri: Uri? = null
+    private var filepath_uri: Uri? = null
+    private lateinit var bitMap:Bitmap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,6 +22,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.btSelectImage.setOnClickListener {
             mGetContent.launch("image/*")
+        }
+
+        binding.btInsert.setOnClickListener {
+
+
         }
     }
 
@@ -29,6 +37,20 @@ class MainActivity : AppCompatActivity() {
                 .load(uri)
                 .centerCrop()
                 .into(binding.ivImage);
+            filepath_uri = uri
+
+            val input_stream= uri?.let { contentResolver.openInputStream(it) }
+            bitMap=BitmapFactory.decodeStream(input_stream)
+
+            imageStore(bitMap)
+
 
         }
+
+    private fun imageStore(bitMap: Bitmap?) {
+
+        val stream=ByteArrayOutputStream()
+        bitMap?.compress(Bitmap.CompressFormat.JPEG,100,stream)
+
+    }
 }
